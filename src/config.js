1de6f833a15d26aedb6ba3b23d5d190107f1e508
pierrekzo <pierrecasaux@gmail.com>
2020-01-02 17:55:51 +0100

    config.js pour heroku

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "test",
-  password: "Password95#",
-  database: "ouiexpat"
+  host: "eu-cdbr-west-02.cleardb.net",
+  user: "bda365288edd60",
+  password: "5a65d830",
+  database: "heroku_413d8abfe34321f"
 });
 module.exports = connection;
