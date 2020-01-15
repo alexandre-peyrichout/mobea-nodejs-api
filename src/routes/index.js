1fa1f695e9827755b2b2e5fb617bf5d3eaf25a67
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-01-15 16:14:12 +0100

    authentification

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,28 @@
 const express = require("express");
 
-const reason = require('./reason');
-const taskType = require('./taskType');
+const reason = require("./reason");
+const taskType = require("./taskType");
 const city = require("./city");
-const admin = require('./admin');
+const admin = require("./admin");
 const country = require("./country");
-const task = require('./task');
+const task = require("./task");
 const user = require("./user");
-const destination = require('./destination');
+const dashboard = require("./dashboard");
+const destination = require("./destination");
 const situation = require("./situation");
-const taskHasDestination = require('./taskHasDestination');
+const taskHasDestination = require("./taskHasDestination");
 
 const router = express.Router();
 
-router.use('/reason', reason);
-router.use('/taskType', taskType);
+router.use("/reason", reason);
+router.use("/taskType", taskType);
 router.use("/city", city);
-router.use('/admin', admin);
-router.use('/task', task);
-
-router.use('/destination', destination);
+router.use("/admin", admin);
+router.use("/task", task);
+router.use("/dashboard", dashboard);
+router.use("/destination", destination);
 router.use("/situation", situation);
-router.use('/taskHasDestination', taskHasDestination);
+router.use("/taskHasDestination", taskHasDestination);
 
 router.use("/user", user);
 router.use("/country", country);
