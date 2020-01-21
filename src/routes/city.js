11f6587bd921d185893a110c428342e69e028f5c
psycho33200 <pininfarina47@gmail.com>
2020-01-21 14:09:00 +0100

    modif cors

--- src/routes/city.js
+++ src/routes/city.js
@@ -16,7 +16,7 @@ router.get("/", (req, res) => {
 // Create a new city
 router.post("/new", (req, res) => {
   const formData = req.body;
-  connection.query("INSERT INTO city SET ?", formData, (err) => {
+  connection.query("INSERT INTO city SET ?", formData, err => {
     if (err) {
       res.status(500).send("Error create new city");
     } else {
@@ -29,22 +29,27 @@ router.post("/new", (req, res) => {
 router.put("/:id", (req, res) => {
   const idUrl = req.params.id;
   const formData = req.body;
+  console.log(idUrl, formData);
 
-  connection.query("UPDATE city SET ? WHERE idcity = ?", [ formData, idUrl ], (err) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.sendStatus(200);
+  connection.query(
+    "UPDATE city SET ? WHERE idcity = ?",
+    [formData, idUrl],
+    err => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.sendStatus(200);
+      }
     }
-  });
+  );
 });
 
 // Route to Delete ONE city
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   const idUrl = req.params.id;
-  connection.query('DELETE FROM city WHERE idcity = ?', [idUrl], (err) => {
+  connection.query("DELETE FROM city WHERE idcity = ?", [idUrl], err => {
     if (err) {
-      res.status(500).send('Error deleting');
+      res.status(500).send("Error deleting");
     } else {
       res.sendStatus(200);
     }
