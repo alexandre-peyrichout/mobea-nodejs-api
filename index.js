bc0357c1203a9a49d6b8e78323894401e6c0841f
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-02-04 16:36:29 +0100

    add req headers

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.all("*", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
+  req.header("Access-Control-Allow-Origin", "*");
+  req.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  req.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
