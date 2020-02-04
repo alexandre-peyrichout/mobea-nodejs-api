f5cc1d562f9400b3fae87fd2ee77beda3979b83c
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-02-04 16:52:37 +0100

    up app use req headers

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const bodyParser = require("body-parser");
 const connection = require("./src/config");
 const api = require("./src/routes");
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+
+  next();
+});
+
 app.use("/api", api);
 
 // Support JSON-encoded bodies
@@ -14,12 +25,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Dans index.js ajouter config CORS
-app.all("*", (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 
 app.listen(PORT, err => {
   if (err) {
