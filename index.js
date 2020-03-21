1b10832d8dcac1698898880684672af1e0b61c96
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-03-21 19:06:08 +0100

    [FIX] cors

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require("express");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-const api = require("./src/routes");
+const dotenv = require("dotenv");
+dotenv.config();
 
-// pour plaire au corse
+const bodyParser = require("body-parser");
+const api = require("./src/routes");
+const port = process.env.PORT || 3000;
+const app = express();
+
+
+// Headers
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    "Origin, X-Requested-With, Content-Type, Accept, authorization"
   );
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type,**Authorization**");
-
+  res.header("Access-Control-Allow-Methods", "POST, PUT, DELETE, GET, OPTIONS");
+  // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
     res.send(200);
   }
@@ -23,26 +26,18 @@ app.use((req, res, next) => {
   }
 });
 
-const bodyParser = require("body-parser");
-
-const cors = require("cors")
 
 // Support JSON-encoded bodies
 app.use(bodyParser.json());
 // Support URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
-
 
 app.use("/api", api);
 
-// Dans index.js ajouter config CORS
-
-app.listen(PORT, err => {
-
+app.listen(port, err => {
   if (err) {
     throw new Error("Something bad happened...");
   }
-  console.log(`Server is listening on ${PORT}`);
+  console.log(`Server is listening on ${port}`);
 });
